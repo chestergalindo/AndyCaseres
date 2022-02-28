97a5531f82f994209ba67de9269a8ef7296a83e7
Andy Caseres <andycaseres2@gmail.com>
2022-02-27 22:22:12 -0500

    modificated sidebarmenu media 375px

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,35 +1,44 @@
-import SocialNet from '../../components/SocialNet'
-import './About.scss'
+import SocialNet from "../../components/SocialNet";
+import "./About.scss";
 
 const About = () => {
   return (
-    <main className='About'>
-      <div className='about-inf'>
-        <h1 className='left-h1'>SOBRE MI</h1>
-        <div className='left'>
+    <main className="About">
+      <div className="about-inf">
+        <h1 className="left-h1">SOBRE MI</h1>
+        <div className="left">
           <img
-            className='img'
-            src='https://i.ibb.co/RyJHmxR/andy.png'
-            alt='Andy'></img>
-          <div className='cont-left'></div>
+            className="img"
+            src="https://i.ibb.co/RyJHmxR/andy.png"
+            alt="Andy"
+          ></img>
+          <div className="cont-left"></div>
         </div>
 
-        <div className='right'>
-          <div className='cont-right'>
+        <div className="right">
+          <div className="cont-right">
             <h3>
               Soy <span>Andy!</span>
             </h3>
+            <p>
+              🖥️ Soy un desarrollador web autodidacta orientado al Front-End.
+            </p>
             <span>
-              Soy un Joven Apasionado por el Desarrollo Web, soy autodidacta
-              Tengo un muy buen nivel de aprendizaje. Espero poder siempre dar
-              lo mejor de mi y seguir aprendiendo nuevas tecnologias.
+              🔎 Actualmente me encuentro en búsquedas de posiciones Front End
+              Junior
             </span>
+            <p>
+              ⚡ Me caracterizo por ser una persona dedicada, proactiva,
+              responsable y sólida para cumplir objetivos. Con ganas de poder
+              brindar mis conocimientos en un entorno laboral, y poder
+              desarrollarme en un equipo.
+            </p>
           </div>
           <SocialNet />
         </div>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default About
+export default About;
